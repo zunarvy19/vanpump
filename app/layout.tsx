86bcb1282f948f0fc8aad19c2db88e5bbc99a906
zunarvy19 <arvy977.gmail.com>
2026-07-17 14:37:10 +0700

    feat: floatin actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import FloatingActions from "@/components/FloatingActions";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -112,6 +113,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <FloatingActions />
       </body>
     </html>
   );
